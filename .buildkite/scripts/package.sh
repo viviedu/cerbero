fb51083ca33276c36c13079a7b69c429acbeb8d6
Jake Barnes <jake@vivi.io>
2020-04-24 15:11:33 +1000

    Update filenames

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 ./cerbero-uninstalled -c config/cross-android-universal.cbc package gstreamer-1.0
-cp gstreamer-1.0-android-universal-1.16.1.tar.bz2 "artifacts/${VIVI_FILENAME}"
+cp gstreamer-1.0-android-universal-1.16.2.tar.bz2 "artifacts/${VIVI_FILENAME}"
