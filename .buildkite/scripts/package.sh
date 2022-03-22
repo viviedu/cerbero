1ac41edc812c2917e52ff8dfe5c7b95ee9e764cd
Andrew Pritchard <andrew@vivi.io>
2022-03-22 15:09:29 +1100

    Upgrade to 1.20.1 instead of 1.20.0 for libsoup VIVI-7003 - there were a bunch of fixes for libsoup on android

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 ./cerbero-uninstalled -c config/cross-android-universal.cbc package gstreamer-1.0
-cp gstreamer-1.0-android-universal-1.20.0.tar.xz "artifacts/${VIVI_FILENAME}"
+cp gstreamer-1.0-android-universal-1.20.1.tar.xz "artifacts/${VIVI_FILENAME}"
