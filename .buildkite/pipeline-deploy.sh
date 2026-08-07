318f5a263928f38637b48f369c2f474ff9deaeea
Phillip Abbott <phillipa@vivi.io>
2026-08-07 10:23:51 +1000

    fix: update queue to one that hasnt been nuked

--- .buildkite/pipeline-deploy.sh
+++ .buildkite/pipeline-deploy.sh
@@ -12,7 +12,7 @@ steps:
     depends_on:
       - package
     agents:
-      queue: v3
+      queue: v6
     command: .buildkite/scripts/links.sh ${VIVI_FILENAME}
     plugins:
       - artifacts#309df16:
